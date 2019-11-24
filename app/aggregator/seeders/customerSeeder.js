3d95a7df1f198d8080fdf03d1cd23bfa7c6eeed3
Francisco Almeida <franciscocfalmeida@gmail.com>
2019-11-24 17:32:00 +0000

    implement customerSeeder

--- app/aggregator/seeders/customerSeeder.js
+++ app/aggregator/seeders/customerSeeder.js
@@ -1,1 +1,19 @@
-const saft = require('../saft');
+const jp = require('jsonpath');
+const Customer = require('../models/customer');
+
+async function seed(data) {
+  const customers = jp.query(data, '$.auditFile.masterFiles.customer')[0];
+
+  for (i in customers) {
+    const customer = customers[i];
+    
+    await Customer.create({
+      customerId: customer.customerId,
+      companyName: customer.companyName
+    });
+  }
+}
+
+module.exports = {
+  seed
+}
