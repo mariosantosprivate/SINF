58cbe5369444cc4e4c651caffd65612ffa726aa9
Francisco Almeida <franciscocfalmeida@gmail.com>
2019-12-04 22:39:32 +0000

    add salesTrend helper

--- app/dashboard-api/services/sales/salesTrend.js
+++ app/dashboard-api/services/sales/salesTrend.js
@@ -1,6 +1,25 @@
-// TODO: implement this method
+const getMonth = require('date-fns/getMonth');
+const Invoice = require('../../../common/models/invoice');
+
 async function calculate(fiscalYear) {
-  return null;
+  let salesPerMonth = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+  const invoices = await Invoice.findAll({
+    raw: true,
+    where: {
+      fiscal_year: fiscalYear,
+    },
+  });
+
+  for (const invoice of invoices) {
+    const month = getMonth(new Date(invoice.invoiceDate));
+
+    salesPerMonth[month] += parseFloat(invoice.netTotal);
+  }
+
+  salesPerMonth = salesPerMonth.map((value) => Number(value.toFixed(2)));
+
+  return salesPerMonth;
 }
 
 module.exports = calculate;
