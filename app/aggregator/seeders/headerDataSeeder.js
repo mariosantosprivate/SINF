10a332c1e4c454816dd91edd9ad19d94ee971092
Francisco Almeida <franciscocfalmeida@gmail.com>
2019-12-09 22:14:02 +0000

    fix headerDataSeeder error when trying to get currencyCode

--- app/aggregator/seeders/headerDataSeeder.js
+++ app/aggregator/seeders/headerDataSeeder.js
@@ -2,13 +2,18 @@ const HeaderData = require('../../common/models/headerData');
 
 async function seed(data) {
   const headerData = data.auditFile.header;
+  let currencyCode;
+
+  if (typeof headerData.currencyCode === 'object') {
+    currencyCode = headerData.currencyCode._;
+  } else { currencyCode = headerData.currencyCode; }
 
   await HeaderData.create({
     fiscalYear: headerData.fiscalYear,
     companyName: headerData.companyName,
     startDate: headerData.startDate,
     endDate: headerData.endDate,
-    currencyCode: headerData.currencyCode,
+    currencyCode,
   });
 }
 
