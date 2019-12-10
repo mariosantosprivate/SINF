cbdd75aaab2ba1e91ba408cb48f2c775ce6550cd
Francisco Almeida <franciscocfalmeida@gmail.com>
2019-12-10 23:09:57 +0000

    make it possible to select fiscal year in sales dashboard

--- app/dashboard-app/pages/sales.js
+++ app/dashboard-app/pages/sales.js
@@ -6,7 +6,10 @@ import BarChart from '../components/Chart/BarChart';
 import PieChart from '../components/Chart/PieChart';
 import KPI from '../components/KPI';
 import salesService from '../services/salesService';
+import metadataService from '../services/metadataService';
 import chartTypes from '../utils/chartTypes';
+import FiscalYearSelect from '../components/FiscalYearSelect';
+import ErrorMessage from '../components/ErrorMessage';
 // Importing a local CSS file.
 import '../assets/css/style.css';
 
@@ -17,68 +20,110 @@ class Sales extends React.Component {
     this.state = {
       data: null,
       loading: false,
+      hasError: false,
+      errorMessage: null,
     };
 
     this.fetchData = this.fetchData.bind(this);
+    this.onChangeFiscalYear = this.onChangeFiscalYear.bind(this);
   }
 
   componentDidMount() {
-    this.fetchData();
+    this.fetchData(null);
   }
 
-  async fetchData() {
+  async onChangeFiscalYear(event) {
+    this.fetchData(event);
+  }
+
+  async fetchData(_fiscalYear) {
+    let fiscalYear = _fiscalYear;
+
     this.setState({ loading: true });
-    const data = await salesService.getMetrics(2016);
-    this.setState({ loading: false, data });
+
+    try {
+      if (!fiscalYear) {
+        const fiscalYears = await metadataService.getFiscalYears();
+        [fiscalYear] = fiscalYears;
+      }
+    } catch (err) {
+      this.setState({ hasError: true, errorMessage: err.message });
+    }
+
+    try {
+      const data = await salesService.getMetrics(fiscalYear);
+      this.setState({
+        loading: false,
+        data,
+        hasError: false,
+        errorMessage: null,
+      });
+    } catch (err) {
+      this.setState({ hasError: true, errorMessage: err.message });
+    }
   }
 
   render() {
-    if (!this.state.data) return null;
-
-    if (this.state.loading) return 'Loading...';
-
     const { data } = this.state;
 
-    return (
+    const header = (
       <div>
-        <Header />
-        <div>
-          <Row className="justify-content-center top-padded-row side-padded-row-small">
-            <Col lg={12} className="text-center">
-              <KPI title="Sales value" value={`${data.totalSalesNetValue.toLocaleString()} €`} />
-            </Col>
-          </Row>
-          <Row className="text-center center-content top-padded-row side-padded-row">
-            <Col className="text-center w-50">
-              <BarChart
-                data={data.topSoldProducts.map((product) => product.value)}
-                labels={data.topSoldProducts.map((product) => product.name)}
-                legend="Top 5 sold products"
-              />
-            </Col>
-            <Col className="text-center w-50">
-              <BarChart
-                data={data.topCustomers.map((customer) => customer.value)}
-                labels={data.topCustomers.map((customer) => customer.name)}
-                legend="Top 5 customers"
-              />
-            </Col>
-          </Row>
-          <Row className="justify-content-sm-center top-padded-row side-padded-row">
-            <Col className="text-center">
-              <TrendChart legend="Sales trend" data={this.state.data.salesTrend} />
-            </Col>
-            <Col className="text-center">
-              <PieChart
-                data={data.salesByRegion.map((region) => region.value)}
-                labels={data.salesByRegion.map((region) => region.name)}
-                chartType={chartTypes.EURO_CHART}
-              />
-            </Col>
-          </Row>
-        </div>
+        <Header>
+          <FiscalYearSelect onChange={this.onChangeFiscalYear} />
+        </Header>
       </div>
     );
+
+    const page = [header];
+
+    if (this.state.hasError) {
+      page.push(<ErrorMessage message={this.state.errorMessage} />);
+      return page;
+    }
+
+    if (!this.state.data || this.state.loading) return page;
+
+    const content = (
+      <div>
+        <Row className="justify-content-center top-padded-row side-padded-row-small">
+          <Col lg={12} className="text-center">
+            <KPI title="Sales value" value={`${data.totalSalesNetValue.toLocaleString()} €`} />
+          </Col>
+        </Row>
+        <Row className="text-center center-content top-padded-row side-padded-row">
+          <Col className="text-center w-50">
+            <BarChart
+              data={data.topSoldProducts.map((product) => product.value)}
+              labels={data.topSoldProducts.map((product) => product.name)}
+              legend="Top 5 sold products"
+            />
+          </Col>
+          <Col className="text-center w-50">
+            <BarChart
+              data={data.topCustomers.map((customer) => customer.value)}
+              labels={data.topCustomers.map((customer) => customer.name)}
+              legend="Top 5 customers"
+            />
+          </Col>
+        </Row>
+        <Row className="justify-content-sm-center top-padded-row side-padded-row">
+          <Col className="text-center">
+            <TrendChart legend="Sales trend" data={this.state.data.salesTrend} />
+          </Col>
+          <Col className="text-center">
+            <PieChart
+              data={data.salesByRegion.map((region) => region.value)}
+              labels={data.salesByRegion.map((region) => region.name)}
+              chartType={chartTypes.EURO_CHART}
+            />
+          </Col>
+        </Row>
+      </div>
+    );
+
+    page.push(content);
+
+    return page;
   }
 }
 
