2aa9c495648b4eeb3d818da054c2fca14f953ca8
Ricardofpinto <ricardofpinto98@gmail.com>
2019-12-13 14:37:37 +0000

    Doing the page of the logistics dashboard

--- app/dashboard-app/pages/logistics.js
+++ app/dashboard-app/pages/logistics.js
@@ -1,57 +1,117 @@
-import Card from 'react-bootstrap/Card';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import { DoughnutGraph } from '../components/Chart/DoughnutGraph';
+import React from 'react';
+import { Row, Col } from 'react-bootstrap';
+import KPI from '../components/KPI';
+import PieChart from '../components/Chart/PieChart';
+import logisticsService from '../services/logisticsService';
+import metadataService from '../services/metadataService';
+import FiscalYearSelect from '../components/FiscalYearSelect';
+import ErrorMessage from '../components/ErrorMessage';
 // Importing a local CSS file.
 import '../assets/css/style.css';
 
-const Logistics = () => (
-  <div>
+class Logistics extends React.Component {
+  constructor(_props) {
+    super(_props);
 
-    <div>
+    this.state = {
+      data: null,
+      loading: false,
+      hasError: false,
+      errorMessage: null,
+    };
+
+    this.fetchData = this.fetchData.bind(this);
+    this.onChangeFiscalYear = this.onChangeFiscalYear.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchData(null);
+  }
+
+  async onChangeFiscalYear(event) {
+    this.fetchData(event);
+  }
+
+  async fetchData(_fiscalYear) {
+    let fiscalYear = _fiscalYear;
+
+    this.setState({ loading: true });
+
+    try {
+      if (!fiscalYear) {
+        const fiscalYears = await metadataService.getFiscalYears();
+        [fiscalYear] = fiscalYears;
+      }
+
+      const data = await logisticsService.getMetrics(fiscalYear);
+      this.setState({
+        loading: false,
+        data,
+        hasError: false,
+        errorMessage: null,
+      });
+    } catch (err) {
+      this.setState({ hasError: true, errorMessage: err.message });
+    }
+  }
+
+  render() {
+    const { data } = this.state;
+
+    const fiscalYearSelect = (
       <Row className="justify-content-center top-padded-row side-padded-row-small">
-        <Col className="text-center">
-          <Card style={{ width: '18rem' }} className="center-card">
-            <Card.Body>
-              <Card.Title>Inventory Value</Card.Title>
-              <Card.Text>[VALUE]</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col className="text-center">
-          <Card style={{ width: '18rem' }} className="center-card">
-            <Card.Body>
-              <Card.Title>Average Inventory Period</Card.Title>
-              <Card.Text>[VALUE]</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col className="text-center">
-          <Card style={{ width: '18rem' }} className="center-card">
-            <Card.Body>
-              <Card.Title>Inventory Turnover</Card.Title>
-              <Card.Text>[VALUE]</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col className="text-center">
-          <Card style={{ width: '18rem' }} className="center-card">
-            <Card.Body>
-              <Card.Title>Total Deliveries</Card.Title>
-              <Card.Text>[VALUE]</Card.Text>
-            </Card.Body>
-          </Card>
+        <Col lg={2} className="offset-md-11">
+          <FiscalYearSelect onChange={this.onChangeFiscalYear} />
         </Col>
       </Row>
-      <Row className="text-center center-content top-padded-row side-padded-row">
-        <Col className="text-center w-50">
-          <DoughnutGraph />
-        </Col>
-        <Col className="text-center w-50">
-          <DoughnutGraph />
-        </Col>
-      </Row>
-    </div>
-  </div>
-);
+    );
+
+    const page = [fiscalYearSelect];
+
+    if (this.state.hasError) {
+      page.push(<ErrorMessage message={this.state.errorMessage} />);
+      return page;
+    }
+
+    if (!this.state.data || this.state.loading) return page;
+
+    const content = (
+      <div>
+        <Row className="justify-content-center top-padded-row side-padded-row-small">
+          {/*<Col className="text-center">
+            <KPI title="Inventory Value" value={`${data.totalinventaryValue.toLocaleString()} €`} /> 
+          </Col>
+          <Col className="text-center">
+            <KPI title="Average Inventory Period" value={`${data.averageInventoryPeriod.toLocaleString()} €`} />
+          </Col>
+          <Col className="text-center">
+            <KPI title="Inventory Turnovers" value={`${data.inventoryTurnovers.toLocaleString()} €`} />
+    </Col> */}
+          <Col className="text-center">
+            <KPI title="Total Deliveries" value={`${data.totalDeliveries.toLocaleString()}`} />
+          </Col>
+        </Row>
+        <Row className="justify-content-sm-center top-padded-row side-padded-row">
+          <Col className="text-center">
+            <PieChart
+              data={data.deliveryStatus.map((status) => status.value)}
+              labels={data.deliveryStatus.map((status) => status.name)}
+            />
+          </Col>
+          <Col className="text-center">
+            <PieChart
+              data={data.deliveryByCountry.map((country) => country.value)}
+              labels={data.deliveryByCountry.map((country) => country.name)}
+            />
+          </Col>
+        </Row>
+      </div>
+    );
+
+    page.push(content);
+
+    return page;
+  }
+}
+
 export default Logistics;
