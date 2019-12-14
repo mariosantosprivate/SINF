35476922e1221b17eeedca905ae0a082e5b52956
Miguel Castro <1160574@isep.ipp.pt>
2019-12-14 13:43:13 +0000

    Add to controller and service

--- app/dashboard-api/services/financesService.js
+++ app/dashboard-api/services/financesService.js
@@ -3,8 +3,11 @@ const totalNonCurrentAssets = require('./finances/totalNonCurrentAssets');
 const totalExpenses = require('./finances/totalExpenses');
 const totalIncome = require('./finances/totalIncome');
 const accountsReceivable = require('./finances/accountsReceivable');
+const accountsPayable = require('./finances/accountsPayable');
 const totalRevenue = require('./finances/totalRevenue');
 const revenueTrend = require('./finances/revenueTrend');
+const expensesTrend = require('./finances/expensesTrend');
+const incomeTrend = require('./finances/incomeTrend');
 const financialAutonomy = require('./finances/financialAutonomy');
 
 module.exports = {
@@ -12,8 +15,11 @@ module.exports = {
   totalNonCurrentAssets,
   totalExpenses,
   totalIncome,
+  accountsPayable,
   accountsReceivable,
   totalRevenue,
   revenueTrend,
+  expensesTrend,
+  incomeTrend,
   financialAutonomy,
 };
