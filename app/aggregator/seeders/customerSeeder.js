ad4c54d8f7c6fc530bd66d4484036bc5fd1019c6
Ricardofpinto <ricardofpinto98@gmail.com>
2019-12-18 04:36:55 +0000

    Adding the seeder froom jasmin in customer and invoice

--- app/aggregator/seeders/customerSeeder.js
+++ app/aggregator/seeders/customerSeeder.js
@@ -1,7 +1,9 @@
 const jp = require('jsonpath');
 const Customer = require('../../common/models/customer');
+const axios = require('../requests/axios');
 
-async function seed(data) {
+
+async function seedFromSaft(data) {
   let customers = jp.query(data, '$.auditFile.masterFiles.customer')[0];
 
   if (!Array.isArray(customers)) customers = [customers];
@@ -16,6 +18,29 @@ async function seed(data) {
       });
     }
   }
+}
+
+async function seedFromJasmin(data) {
+  const axiosInstance = axios.getInstance();
+
+  const request = await axiosInstance.get(`salesCore/customerParties/`);
+  const customers = request.data.items;
+
+  for (const key in customers) {
+    if (Object.prototype.hasOwnProperty.call(customers, key)) {
+      const customer = customers[key];
+
+      await Customer.create({
+        customerId: customer.partyKey + "." + customer.companyTaxID,
+        companyName: customer.companyTaxID,
+      });
+    }
+  }
+}
+
+async function seed(data) {
+  await seedFromSaft(data);
+  // await seedFromJasmin(data);
 }
 
 module.exports = {
--- app/aggregator/seeders/invoiceSeeder.js
+++ app/aggregator/seeders/invoiceSeeder.js
@@ -1,9 +1,10 @@
 const jp = require('jsonpath');
+const axios = require('../requests/axios');
 const Invoice = require('../../common/models/invoice');
 const InvoiceLine = require('../../common/models/invoiceLine');
 const ShippingInfo = require('../../common/models/shippingInfo');
 
-async function seed(data) {
+async function seedFromSaft(data) {
   const { fiscalYear } = data.auditFile.header;
 
   const invoices = jp.query(data, '$.auditFile.sourceDocuments.salesInvoices.invoice')[0];
@@ -64,6 +65,79 @@ async function seed(data) {
       }
     }
   }
+}
+
+async function seedFromJasmin(data) {
+  const axiosInstance = axios.getInstance();
+
+  const { fiscalYear } = data.auditFile.header;
+  const request = await axiosInstance.get(`billing/invoices/odata?$filter=year(DocumentDate) eq ${fiscalYear}`);
+  const invoices = request.data.items;
+
+  for (const key in invoices) {
+    if (Object.prototype.hasOwnProperty.call(invoices, key)) {
+      const invoice = invoices[key];
+      if (invoice.buyerCustomerPartyTaxId != null) {
+        const shipTo = await ShippingInfo.create({
+          deliveryDate: invoice.unloadingDateTime,
+          addressDetail: invoice.unloadingPointAddress,
+          city: invoice.unloadingCityName,
+          postalCode: invoice.unloadingPostalZone,
+          country: invoice.unloadingCountry,
+        });
+
+        const shipFrom = await ShippingInfo.create({
+          deliveryDate: invoice.loadingDateTime,
+          addressDetail: invoice.loadingPointAddress,
+          city: invoice.loadingCityName,
+          postalCode: invoice.loadingPostalZone,
+          country: invoice.loadingCountry,
+        });
+        const customerId = `${invoice.buyerCustomerParty}.${invoice.buyerCustomerPartyTaxId}`;
+        try {
+          await Invoice.create({
+            fiscal_year: fiscalYear,
+            invoiceNo: invoice.documentLines[0].invoiceId,
+            invoiceDate: invoice.documentDate,
+            taxPayable: invoice.payableAmountAmount,
+            netTotal: invoice.taxExclusiveAmountAmount,
+            grossTotal: invoice.grossValueAmount,
+            customer_id: customerId,
+            ship_to_info_id: shipTo.id,
+            ship_from_info_id: shipFrom.id,
+          });
+        } catch (err) {
+          //console.log(err);
+        }
+
+
+        let invoiceLines = invoice.documentLines;
+
+        // xml2js doesn't parse lines as an array if there's only 1
+        if (!Array.isArray(invoiceLines)) { invoiceLines = [invoiceLines]; }
+
+        for (const key2 in invoiceLines) {
+          if (Object.prototype.hasOwnProperty.call(invoiceLines, key2)) {
+            const invoiceLine = invoiceLines[key2];
+            const credit_amount = invoiceLine.quantity * invoiceLine.unitPriceAmount;
+            await InvoiceLine.create({
+              invoice_no: invoiceLine.invoiceId,
+              product_code: invoiceLine.salesItem,
+              quantity: invoiceLine.quantity,
+              unitPrice: invoiceLine.unitPriceAmount,
+              description: invoiceLine.description,
+              creditAmount: invoiceLine.taxExclusiveAmountAmount,
+            });
+          }
+        }
+      }
+    }
+  }
+}
+
+async function seed(data) {
+  await seedFromSaft(data);
+  await seedFromJasmin(data);
 }
 
 module.exports = {
