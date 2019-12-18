ab31edb67ac15012385a325ed12b9b838de67f20
Francisco Almeida <franciscocfalmeida@gmail.com>
2019-12-18 01:24:36 +0000

    fix logistics issues caused by changes in ShippingInfo association with Invoice

--- app/dashboard-api/services/logistics/deliveryByCountry.js
+++ app/dashboard-api/services/logistics/deliveryByCountry.js
@@ -3,31 +3,32 @@ const ShippingInfo = require('../../../common/models/shippingInfo');
 const formatTop5 = require('../../utils/formatTop5');
 
 async function calculate(fiscalYear) {
-    const deliveryPerCountry = {};
+  const deliveryPerCountry = {};
 
-    const invoices = await Invoice.findAll({
-        raw: true,
-        where: {
-            fiscal_year: fiscalYear,
-        },
-        include: [{
-            model: ShippingInfo,
-        }],
-    });
+  const invoices = await Invoice.findAll({
+    raw: true,
+    where: {
+      fiscal_year: fiscalYear,
+    },
+    include: [{
+      model: ShippingInfo,
+      as: 'shipToInfo',
+    }],
+  });
 
-    for (const invoice of invoices) {
-        // since the name of the property ShippingInfo.country contains a "." (dot),
-        // the property needs to be fetched like this
-        const country = invoice['ShippingInfo.country'];
+  for (const invoice of invoices) {
+    // since the name of the property ShippingInfo.country contains a "." (dot),
+    // the property needs to be fetched like this
+    const country = invoice['shipToInfo.country'];
 
-        if (!deliveryPerCountry[country]) {
-            deliveryPerCountry[country] = 0;
-        }
-
-        deliveryPerCountry[country]++;
+    if (!deliveryPerCountry[country]) {
+      deliveryPerCountry[country] = 0;
     }
 
-    return formatTop5(deliveryPerCountry);
+    deliveryPerCountry[country] += 1;
+  }
+
+  return formatTop5(deliveryPerCountry);
 }
 
 module.exports = calculate;
--- app/dashboard-api/services/logistics/deliveryStatus.js
+++ app/dashboard-api/services/logistics/deliveryStatus.js
@@ -3,35 +3,36 @@ const ShippingInfo = require('../../../common/models/shippingInfo');
 const formatTop5 = require('../../utils/formatTop5');
 
 async function calculate(fiscalYear) {
-    const delivered = 'delivered';
-    const undelivered = 'undelivered';
-    const deliveryStatus = {};
-    deliveryStatus[delivered] = 0;
-    deliveryStatus[undelivered] = 0;
+  const delivered = 'delivered';
+  const undelivered = 'undelivered';
+  const deliveryStatus = {};
+  deliveryStatus[delivered] = 0;
+  deliveryStatus[undelivered] = 0;
 
-    const invoices = await Invoice.findAll({
-        raw: true,
-        where: {
-            fiscal_year: fiscalYear,
-        },
-        include: [{
-            model: ShippingInfo,
-        }],
-    });
+  const invoices = await Invoice.findAll({
+    raw: true,
+    where: {
+      fiscal_year: fiscalYear,
+    },
+    include: [{
+      model: ShippingInfo,
+      as: 'shipToInfo',
+    }],
+  });
 
-    for (const invoice of invoices) {
-        // since the name of the property ShippingInfo.country contains a "." (dot),
-        // the property needs to be fetched like this
-        let date = new Date(invoice['ShippingInfo.deliveryDate']);
-        let today = new Date();
-        if (date <= today) {
-            deliveryStatus[delivered]++;
-        } else {
-            deliveryStatus[undelivered]++;
-        }
+  for (const invoice of invoices) {
+    // since the name of the property ShippingInfo.country contains a "." (dot),
+    // the property needs to be fetched like this
+    const date = new Date(invoice['shipToInfo.deliveryDate']);
+    const today = new Date();
+    if (date <= today) {
+      deliveryStatus[delivered] += 1;
+    } else {
+      deliveryStatus[undelivered] += 1;
     }
+  }
 
-    return formatTop5(deliveryStatus);
+  return formatTop5(deliveryStatus);
 }
 
 module.exports = calculate;
