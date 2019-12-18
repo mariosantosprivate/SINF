2dca29a0410dbda7cbafd400dfbfcefd4e7a2e2c
Ricardofpinto <ricardofpinto98@gmail.com>
2019-12-18 04:39:10 +0000

    Adding the service to the controller

--- app/dashboard-api/controllers/logisticsController.js
+++ app/dashboard-api/controllers/logisticsController.js
@@ -6,17 +6,22 @@ async function getMetrics(req, res) {
 
     try {
         const totalInventoryValue = await logisticsService.totalInventoryValue();
+        const averageInventoryPeriod = await logisticsService.averageInventoryPeriod(/*firstYear, lastYear*/);
+        const inventoryTurnover = await logisticsService.inventoryTurnover(/*firstYear, lastYear*/);
         const totalDeliveries = await logisticsService.totalDeliveries(fiscalYear);
         const deliveryStatus = await logisticsService.deliveryStatus(fiscalYear);
         const deliveryByCountry = await logisticsService.deliveryByCountry(fiscalYear);
 
         return res.json({
             totalInventoryValue,
+            averageInventoryPeriod,
+            inventoryTurnover,
             totalDeliveries,
             deliveryStatus,
             deliveryByCountry,
         });
     } catch (err) {
+        console.log(err);
         return res.status(500).json(new ErrorMessage());
     }
 }
--- app/dashboard-api/services/logisticsService.js
+++ app/dashboard-api/services/logisticsService.js
@@ -1,10 +1,14 @@
 const totalInventoryValue = require('./logistics/totalInventoryValue');
+const averageInventoryPeriod = require('./logistics/averageInventoryPeriod');
+const inventoryTurnover = require('./logistics/inventoryTurnover');
 const totalDeliveries = require('./logistics/totalDeliveries');
 const deliveryStatus = require('./logistics/deliveryStatus');
 const deliveryByCountry = require('./logistics/deliveryByCountry');
 
 module.exports = {
     totalInventoryValue,
+    averageInventoryPeriod,
+    inventoryTurnover,
     totalDeliveries,
     deliveryStatus,
     deliveryByCountry,
